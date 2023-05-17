bce585992258377f0152dfae09d22d79a1c252b1
Supr0en <erik.ruotsalainen1@gmail.com>
2023-05-18 00:44:21 +0300

    changes and new things added

---
+++ src/javascript/skills.js
@@ -0,0 +1,13 @@
+const skills = [ 'JavaScript', 'TypeScript', 'React', 'React Native', 'NodeJs', 'express', 'HTML', 'CSS', 'SQL' ];
+
+var outputElement = document.getElementById('languages');
+
+for (var i = 0; i < skills.length; i++) {
+    var skill = skills[i];
+
+    var paragraph = document.createElement('span');
+    paragraph.className = 'skill';
+    paragraph.textContent = skill;
+
+    outputElement.appendChild(paragraph);
+}
