b867092d9325f6d0f7303df8e4606fa1d7477ae4
Supr0en <erik.ruotsalainen1@gmail.com>
2023-05-21 12:26:47 +0300

    Recovering working version

--- docs/javascript/skills.js
+++ docs/javascript/skills.js
@@ -1,4 +1,4 @@
-const skills = [ 'JavaScript', 'TypeScript', 'React', 'React Native', 'NodeJs', 'express', 'HTML', 'CSS', 'SQL' ];
+const skills = [ 'JavaScript', 'TypeScript', 'React', 'React Native', 'NodeJs', 'express', 'HTML', 'CSS', 'tailwind' , 'MySQL' ];
 
 var outputElement = document.getElementById('languages');
 
@@ -6,7 +6,7 @@ for (var i = 0; i < skills.length; i++) {
     var skill = skills[i];
 
     var paragraph = document.createElement('span');
-    paragraph.className = 'skill';
+    paragraph.className = 'inline-block bg-primary text-secondary px-2 m-1 rounded-lg whitespace-prewrap hover:opacity-70 hover:cursor-pointer';
     paragraph.textContent = skill;
 
     outputElement.appendChild(paragraph);
